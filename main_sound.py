5ab1dcca600967204f2223ee5269fc8c6444992a
aviresler <aviresler@gmail.com>
2018-01-29 13:26:51 +0200

    minor changes

--- main_sound.py
+++ main_sound.py
@@ -13,14 +13,15 @@ def configure():
 
     # training
     flags.DEFINE_float('initial_learning_rate', 0.01, 'learning rate')
-    flags.DEFINE_integer('num_epochs', 181, 'maximum number of num_epochs')
-    flags.DEFINE_float('lr_schedule', [60.0,120.0,180.0], 'after this amount of epochs, the learning rate is divided by 10')
+    flags.DEFINE_integer('num_epochs', 1000, 'maximum number of num_epochs')
+    flags.DEFINE_float('lr_schedule', [300.0, 600.0], 'after this amount of epochs, the learning rate is divided by 10')
     flags.DEFINE_float('warmup', 10.0, 'for the first warmup epochs, lr is multiplied by 0.1')
     flags.DEFINE_float('momentum', 0.9, 'momentum')
     flags.DEFINE_float('weight_decay', 0.0005, 'weight decay rate')
     flags.DEFINE_integer('k_cross_val', 5, 'number of cross validation sections')
     flags.DEFINE_bool('is_between_class_train', False, 'between class flag')
     flags.DEFINE_string('pretrain_file', '', 'pre-trained model filename')
+    flags.DEFINE_integer('fold', 0, 'cross validation fold, trainig of all epochs will be done according to this split')
 
     # validation
     flags.DEFINE_integer('num_of_valid_crop', 10, 'number of sections for validation cropping')
@@ -28,9 +29,8 @@ def configure():
 
     # prediction / saving outputs for testing or validation
     flags.DEFINE_string('out_dir', 'output', 'directory for saving outputs')
-    flags.DEFINE_bool('is_test_10_crops', False, 'False - regular training flow,  True - 10 crops testing')
-    flags.DEFINE_integer('save_interval', 30, 'number of iterations for saving the model')
-    flags.DEFINE_integer('random_seed', 1234, 'random seed')
+    flags.DEFINE_integer('save_interval', 50.0, 'number of iterations for saving the model')
+    flags.DEFINE_integer('random_seed', 12354, 'random seed')
 
     # data
     flags.DEFINE_string('data_dir', 'dataset/ESC-50-master/audio/', 'path to data directory')
